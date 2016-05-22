cf2abc29bd6ea2d9dba57f29cb4b95fd1aeac460
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2016-05-21 17:19:36 -0700

    Handle GET requests to root

--- server/src/httphandle.py
+++ server/src/httphandle.py
@@ -1,4 +1,10 @@
-import json, http.server, socketserver
+import os, json, http.server, socketserver, sys
+
+rootAddr = os.path.abspath(
+    os.path.dirname(__file__) + '/../../client'
+)
+
+print(rootAddr)
 
 clients = []
 
@@ -8,15 +14,45 @@ class CustomRequestHandler(http.server.BaseHTTPRequestHandler):
             pass
 
     def do_GET(self):
-        if self.path == '/':
+        absPath = rootAddr + self.path
+        if os.path.isfile(absPath):
+            cutoff = self.path.find('/')
+            if cutoff > 0:
+                relaDir = self.path[:cutoff]
+            else:
+                relaDir = self.path
+        else:
+            relaDir = self.path
+        if relaDir == '/':
             clients.append(self.client_address)
-        elif self.path == '/poll':
+        elif relaDir == '/poll':
             pass
+        if os.path.exists(absPath):
+            if os.path.isdir(absPath):
+                absPath = absPath + 'index.html'
+            if os.path.isfile(absPath):
+                self.send_response(200)
+                with open(absPath, 'rb') as f:
+                    data = f.read()
+                    self.wfile.write(data)
+            else:
+                self.send_response(404)
+                self.wfile.write('''<html>
+<head>
+<title>404 Page Not Found</title>
+</head>
+<body>
+<p>Page does not exist</p>
+<p>dipshit</p></body>
+</html>'''.encode('utf-8'))
 
 def run():
     httpd = socketserver.TCPServer(('', 8000),
                                    CustomRequestHandler)
+    httpd.allow_reuse_address = True
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         print('Closing server...')
+    httpd.shutdown()
+    httpd.server_close()
