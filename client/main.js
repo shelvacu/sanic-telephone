398108f983c3f345d352c9464e406a6d080ef60b
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2016-05-21 23:06:49 -0700

    Send description with drawing

--- client/main.js
+++ client/main.js
@@ -16,10 +16,11 @@ $(function(){
     $("#donebutton").click(function(){
 	var img_data = window.sanic_lc.getImage().toDataURL("image/png");
 	img_data = img_data.replace(/^data:image\/(png|jpg);base64,/, "");
+	var desc = $("#imgdesc").val()
 	$.ajax({
 	    url: '/done_img',
 	    method: 'POST',
-	    data: JSON.stringify({img: img_data})
+	    data: JSON.stringify({img: img_data, desc: desc})
 	});
     });
 })
