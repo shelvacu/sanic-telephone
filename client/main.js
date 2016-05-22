de526565b043a088f4db65b39e1e55175a977623
teja sunku <teja sunku>
2016-05-21 23:56:26 -0700

    Fixed a mistake where filepaths were listed incorrectly

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 $(function(){
 	console.log("javascript has run");
-	window.sanic_lc = LC.init($("#destimg").get(0),{imageURLPrefix: '/literallycanvas/img'});//$("#destimg").literallycanvas({imageURLPrefix: 'literallycanvas/img'});
+	window.sanic_lc = LC.init($("#destimg").get(0),{imageURLPrefix: 'literallycanvas/img'});//$("#destimg").literallycanvas({imageURLPrefix: 'literallycanvas/img'});
 	setInterval(function(){
 	$.ajax({
 		url: 'poll',
